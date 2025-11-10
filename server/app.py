a77e0f908e3345b5ad0c456975643cc1266a067d
Jasec <jasec_7@hotmail.com>
2025-11-09 22:51:55 -0800

    complete python p4 buiding get api lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,52 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for baker in Bakery.query.all():
+        bakery_dict = baker.to_dict()
+        bakeries.append(bakery_dict)
+
+    response = make_response(
+        bakeries,
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    baker = Bakery.query.filter(Bakery.id == id).first()
+    
+    baker_dict = baker.to_dict()
+    response = make_response(
+       baker_dict,
+       200 
+    )
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baker = BakedGood.query.order_by(BakedGood.price.desc()).all()
+
+    price_desc = [price.to_dict() for price in baker]
+    response = make_response(
+        price_desc,
+        200
+    )
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baker = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+
+    price_dict = baker.to_dict()
+    response = make_response(
+        price_dict,
+        200
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
